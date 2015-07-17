a94ffd76dc1895300502e674b4c2a1f4ea4e8632
Edouard Payne <ejpayne@hotmail.co.uk>
2015-07-17 19:38:49 +0100

    Added the location menu

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,7 @@
 class Location < ActiveRecord::Base
+	validates :city, presence: true
+	validates :venue, presence: true
+	validates :when, presence: true
+	validates :start_time, presence: true
+	validates :end_time, presence: true
 end
