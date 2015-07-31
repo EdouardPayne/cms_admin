ea8a15738c677bdcefa8051ee0233699a2777933
Edouard Payne <ejpayne@hotmail.co.uk>
2015-07-31 14:47:21 +0100

    Changed CSS/HTML

---
+++ app/assets/javascripts/target_blank.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+// UPDATE REL="EXTERNAL" TO TARGET="_BLANK" - XHTML 1.1 STRICT DISALLOWS TARGET
+function externalLinks() {
+ if (!document.getElementsByTagName) return;
+ var anchors = document.getElementsByTagName("a");
+ for (var i=0; i<anchors.length; i++) {
+   var anchor = anchors[i];
+   if (anchor.getAttribute("href") &&
+       anchor.getAttribute("rel") == "external")
+     anchor.target = "_blank";
+ }
+}
+window.onload = externalLinks;
